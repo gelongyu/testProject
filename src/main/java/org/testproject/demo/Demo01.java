8ca51c53dbec31e75b6b5d9df0017003a96e661c
gly <gelongyu@qq.com>
2021-05-27 22:14:22 +0800

    我家傻狍子第一次

--- src/main/java/org/testproject/demo/Demo01.java
+++ src/main/java/org/testproject/demo/Demo01.java
@@ -1,9 +1,11 @@
 package org.testproject.demo;
 
 public class Demo01 {
+    public static final int NAME_AGE = 20;
+    final public static String LILIANJIE = "是傻狍子";
 
     public static void main(String[] args) {
-        System.out.println("李四1");
+        System.out.println("李莲洁" + LILIANJIE);
     }
 
 
